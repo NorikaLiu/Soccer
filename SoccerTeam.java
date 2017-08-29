9d8bb95c3a6de71e4bd7c0ec9bfe412daedb7c67
NorikaLiu <31222327+NorikaLiu@users.noreply.github.com>
2017-08-29 08:01:07 -1000

    Add files via upload

---
+++ SoccerTeam.java
@@ -0,0 +1,33 @@
+
+public class SoccerTeam {
+	private static int numberOfPoint = 0;
+	double myScore;
+	double opponentscore;
+	
+	public void myScore () {
+		myScore++;	
+
+	}
+	public void opponentScore () {
+		opponentscore++;	
+
+	}3262
+	
+		
+public void played (SoccerTeam opponent, int myScore, int opponentScore) {
+	
+	
+}
+//public static void main (String args [] ){ 
+	
+	
+//}
+public static int getNumberofPoints() {
+	return numberOfPoint;
+}
+
+
+	
+	
+
+}
