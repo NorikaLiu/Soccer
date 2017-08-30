42144eedaf5e1633879305f5e21a2fc432971a3e
NorikaLiu <31222327+NorikaLiu@users.noreply.github.com>
2017-08-30 06:21:01 -1000

    Add files via upload

--- Points.java
+++ Points.java
@@ -1,4 +1,44 @@
 
-public class Points {
+public class Points extends SoccerTeam {
+	public Points(int numberOfWins, int numberOfLose, int numberofTies) {
+		super(numberOfWins, numberOfLose, numberofTies);
+		// TODO Auto-generated constructor stub
+	}
 
+	public static void main(String[] args) {
+		System.out.println ("Play");
+		
+		SoccerTeam Game1 = new SoccerTeam (1, 1, 1);
+		SoccerTeam Game2 = new SoccerTeam (3, 6 , 7);
+		SoccerTeam Game3 = new SoccerTeam (6, 8 , 9 );
+		SoccerTeam Game4 = new SoccerTeam (2, 8 , 9);
+		
+		SoccerTeam.startTournament ();
+		
+		System.out.println("First");
+		
+		System.out.println("Number of Games Play: 4");
+
+		System.out.println("Total Games played is " +SoccerTeam.Games());
+
+		System.out.println( "First Team ");
+
+		System.out.println( " Points: " + Game1.getNumberOfPoints());
+		
+		System.out.println( "Second Team ");
+
+		System.out.println( " Points: " + Game2.getNumberOfPoints());
+		System.out.println( "Third Team ");
+
+		System.out.println( " Points: " + Game3.getNumberOfPoints());
+		
+		System.out.println( "Last Team ");
+
+		System.out.println( " Points: " + Game4.getNumberOfPoints());
+		
+		Game1.reset (); 
+		Game2.reset (); 
+		Game3.reset (); 
+		Game4.reset (); 
+	}
 }
--- SoccerTeam.java
+++ SoccerTeam.java
@@ -1,32 +1,74 @@
 
 public class SoccerTeam {
-	private static int numberOfPoint = 0;
-	double myScore;
-	double opponentscore;
+	private int numberOfPoint = 0;
+	private int numberofTies = 0;
+	private int numberOfWins = 0;
+	private int numberOfLose = 0;
+	private int myScore;
+	private int opponentscore;
+	private static int game;
+	public static int Goals;
 	
-	public void myScore () {
-		myScore++;	
+	//public void myScore () {
+	//	myScore++;	
 
-	}
-	public void opponentScore () {
-		opponentscore++;	
+//	}
+//	public void opponentScore () {
+	//	opponentscore++;	
 
-	}3262
-	
+	//}
+	public static void startTournament (){ 
+		game = 0;
+		Goals= 0;
 		
-public void played (SoccerTeam opponent, int myScore, int opponentScore) {
+		
+	}
+	public SoccerTeam (int numberOfWins, int numberOfLose, int numberofTies) {
+		this.numberOfWins = numberOfWins;
+		this.numberOfLose = numberOfLose;
+		this.numberofTies = numberofTies;
+		
+	}
+		
+public void played ( SoccerTeam opponent,  int myScore, int opponentscore) {
+	game++
+	
+	;
+Goals = Goals + myScore + opponentscore;
+
+	if  (myScore > opponentscore) {
+		numberOfWins++;
+	}
+	if  (opponentscore > myScore) {
+		numberOfLose++;
+	}
+	if  (opponentscore == myScore) {
+		numberofTies++;
+	}
+
+}
+public static int Games () {
+	return game;
 	
 	
+	
+} 
+public static int Goal () {
+	return Goals;
 }
 //public static void main (String args [] ){ 
 	
 	
 //}
-public static int getNumberofPoints() {
-	return numberOfPoint;
+public int getNumberOfPoints() {
+
+	numberOfPoint = (numberOfWins * 3) + numberofTies;
+	return numberOfPoint; 
 }
-
-
+public void reset () {
+	numberOfPoint = 0;
+	
+}
 	
 	
 
